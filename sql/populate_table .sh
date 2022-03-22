8f08437aa0ec02b69eacd19b0f8ee201750d64ec
Krzysztof Kopieczek <krzysztof.kopieczek@chmurowisko.pl>
2022-03-22 20:15:33 +0100

    Nie trzeba wykorzystywać bucket-a

--- sql/populate_table .sh
+++ sql/populate_table .sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -ne 2]
 then
-    echo "Usage: ./populate_table.sh  bucket-name"
+    echo "Usage: ./populate_table.sh  database-ip  password"
     exit
 fi
 
-BUCKET=$1
-echo "Populating Cloud SQL instance flights from gs://${BUCKET}/flights/raw/..."
+SOURCE=$HOME/flights-raw/
+echo "Populating Cloud SQL instance flights from $SOURCE"
 
 # the table name for mysqlimport comes from the filename, so rename our CSV files, changing bucket name as needed
+# and that's why csvs should be copied
+
 counter=0
+FILES="${SOURCE}/*.csv"
 
-for FILE in 201501.csv 201507.csv; do
-    gsutil cp gs://${BUCKET}/flights/raw/$FILE flights.csv-${counter}
-    counter=$((counter+1))
+DBIP=$1
+PASS=$2
+
+for FILE in $FILES; do
+    echo "Populating $FILE"
+    cp $FILE flights.csv
+
+    cat flights.csv | PGPASSWORD=$PASS psql --host=$DBIP --user=postgres -d postgres -c "\copy flights from stdin delimiter ',' csv header;"
+    rm flights.csv
 done
-
-# import csv files
-
-cat flights.csv-* | PGPASSWORD=$PASS psql --host=$DBIP --user=postgres -d postgres -c "\copy flights from stdin delimiter ',' csv header;"
-
-rm flights.csv-*
